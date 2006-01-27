1899f71cf4240b4208219fb8913106d2c39d4ac8
sembrosk <sembrosk>
2006-01-27 15:06:21 +0000

    GHS

---
+++ kassrtmrg/include/kassrtmrg_command_line.h
@@ -0,0 +1,7 @@
+!KASSRTMRG Command line options  
+        character*120  input_par_file_name !-p
+        character*120  pe_input_file !-i
+        character*120  pes_output_file !-o
+        character*120  pes_hdf5_output_file !-h
+       common/cmdlineinput/input_par_file_name,pe_input_file,
+     1        pes_output_file,pes_hdf5_output_file
