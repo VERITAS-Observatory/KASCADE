fad03c9cf41f58a9a5ba5c41193dbeafe29d235c
sembrosk <sembrosk>
2006-04-25 15:13:04 +0000

    GHS: cleanup

--- common/src/datetime.f90
+++
@@ -1,9 +0,0 @@
-    SUBROUTINE DATETIME(adate,time)
-!****************************************************************************
-!	F77 interface to f90 routine date_and_time
-!****************************************************************************
-	character(len=8) :: adate
-	character(len=10) ::  time          	
-	call date_and_time(adate,time)
-	return
-   END SUBROUTINE DATETIME
