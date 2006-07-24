ca5f3cc30a37d8f2a2c23214ab10b9336c885375
sembrosk <sembrosk>
2006-07-24 19:01:41 +0000

    GHS: Fixinf veritas peds and simulaiton head and ttrees

--- scripts/mkHillasPlots.C
+++ scripts/mkHillasPlots.C
@@ -2,8 +2,17 @@
 void HillasPlots(char* fFileName)
 {
   TFile fHillasFile(fFileName," Input Hillas File");
-     TTree* fHParTree=
-       (TTree*)fHillasFile.Get("ParameterisedEvents/ParEventsTree");
+  std::string fBasicCuts=
+                     "H.fGoodImage && H.fPixelsInImage>2 &&H.fTriggerCode==1";
+  TTree* pfHParTree=NULL;
+  pfHParTree=(TTree*)fHillasFile.Get("ParameterisedEvents/ParEventsTree");
+
+  if(pfHParTree==NULL)
+    {
+      pfHParTree=(TTree*)fHillasFile.Get("ParameterisedEvents/ParEventTree");
+      fBasicCuts="H.fGoodImage && H.fPixelsInImage>2";
+    }
+	 
   TCanvas* fC1 = new TCanvas("fC1","Hillas Params pg1",600,700);
   TCanvas* fC2 = new TCanvas("fC2","Hillas Params pg2",600,700);
   TCanvas* fC3 = new TCanvas("fC3","Hillas Params pg3",600,700);
@@ -14,82 +23,83 @@ void HillasPlots(char* fFileName)
   fC4->Divide(2,2);
   
   fC1->cd(1);
-  fHParTree->Draw("H.fWidth",
-		  "H.fGoodImage && H.fTriggerCode==1 && H.fWidth>0.0 "
-		  "&& H.fWidth<.5 && H.fPixelsInImage>2");
+  std::string fCuts=fBasicCuts + "&& H.fWidth>0.0 && H.fWidth<.5" ;
+
+  pfHParTree->Draw("H.fWidth",fCuts.c_str());
+
+
   fC1->cd(2);
-  fHParTree->Draw("H.fLength",
-		  "H.fGoodImage && H.fTriggerCode==1 && H.fLength>0.0 " 
-		  "&& H.fLength<.8 && H.fPixelsInImage>2");
+  fCuts=fBasicCuts + "&& H.fLength>0.0 && H.fLength<.8";
+  pfHParTree->Draw("H.fLength",fCuts.c_str());
+
   fC1->cd(3);
-  fHParTree->Draw("H.fMiss",
-		  "H.fGoodImage && H.fTriggerCode==1 && H.fMiss>0.0 "
-                  "&& H.fMiss<1.75 && H.fPixelsInImage>2");
+  fCuts=fBasicCuts + "&& H.fMiss>0.0 && H.fMiss<1.75"; 
+  pfHParTree->Draw("H.fMiss",fCuts.c_str());
+
   fC1->cd(4);
-  fHParTree->Draw("H.fDist",
-		  "H.fGoodImage && H.fTriggerCode==1 && H.fDist>0.0 " 
-                  " && H.fDist<1.75 && H.fPixelsInImage>2");
+  fCuts=fBasicCuts + "&& H.fDist>0.0 && H.fDist<1.75"; 
+  pfHParTree->Draw("H.fDist",fCuts.c_str());
+
   fC1->cd(5);
-  fHParTree->Draw("H.fAlpha"," H.fGoodImage && H.fTriggerCode==1 " 
-                  "&& H.fAlpha>0 && H.fPixelsInImage>2");
+  fCuts=fBasicCuts + "&&H.fAlpha>0"; 
+  pfHParTree->Draw("H.fAlpha",fCuts.c_str());
+
   fC1->cd(6);
-  fHParTree->Draw("H.fSize",
-		  "H.fGoodImage && H.fTriggerCode==1 && H.fSize>0.0 " 
-                  "&& H.fSize<2000 && H.fPixelsInImage>2");
+  fCuts=fBasicCuts + "&& H.fSize>0.0 && H.fSize<2000";  
+    pfHParTree->Draw("H.fSize",fCuts.c_str());
 
   fC2->cd(1);
-  fHParTree->Draw("H.fLengthOverSize",
-		  "H.fGoodImage && H.fTriggerCode==1 "
-		  "&& H.fLengthOverSize>0.0 && H.fLengthOverSize<.0015 && "
-		  "H.fPixelsInImage>2");
+  fCuts=fBasicCuts + "&& H.fLengthOverSize>0.0 && H.fLengthOverSize<.0015";
+  pfHParTree->Draw("H.fLengthOverSize",fCuts.c_str());
+
   fC2->cd(2);
-  fHParTree->Draw("H.fAzWidth",
-		  "H.fGoodImage && H.fTriggerCode==1 && H.fAzWidth>0.0 " 
-                  " && H.fAzWidth<.8 && H.fPixelsInImage>2");
+  fCuts=fBasicCuts + "&& H.fAzWidth>0.0 && H.fAzWidth<.8 ";
+  pfHParTree->Draw("H.fAzWidth",fCuts.c_str());
+
   fC2->cd(3);
-  fHParTree->Draw("H.fMax1","H.fGoodImage && H.fTriggerCode==1 "
-		  "&& H.fMax1>0.0 && H.fMax1<200 && H.fPixelsInImage>2");
+  fCuts=fBasicCuts + "&& H.fMax1>0.0 && H.fMax1<200";
+  pfHParTree->Draw("H.fMax1",fCuts.c_str());
+
   fC2->cd(4);
-  fHParTree->Draw("H.fMax2","H.fGoodImage && H.fTriggerCode==1 "
-		  "&& H.fMax2>0.0 && H.fMax2<200 && H.fPixelsInImage>2");
+  fCuts=fBasicCuts + "&& H.fMax2>0.0 && H.fMax2<200";
+  pfHParTree->Draw("H.fMax2",fCuts.c_str());
+
   fC2->cd(5);
-  fHParTree->Draw("H.fMax3","H.fGoodImage && H.fTriggerCode==1 "
-		  "&& H.fMax3>0.0 && H.fMax3<200 && H.fPixelsInImage>2");
+  fCuts=fBasicCuts +"&& H.fMax3>0.0 && H.fMax3<200"; 
+  pfHParTree->Draw("H.fMax3",fCuts.c_str());
+
   fC2->cd(6);
-  fHParTree->Draw("H.fFrac1",
-		  "H.fGoodImage && H.fTriggerCode==1 && H.fFrac1>0 && "
-		  "H.fPixelsInImage>2");
+  fCuts=fBasicCuts + "&& H.fFrac1>0 "; 
+  pfHParTree->Draw("H.fFrac1",fCuts.c_str());
+
   
   fC3->cd(1);
-  fHParTree->Draw("H.fFrac2",
-		  "H.fGoodImage && H.fTriggerCode==1 && H.fFrac2>0 && "
-		  "H.fPixelsInImage>2");
+  fCuts=fBasicCuts + "&& H.fFrac2>0"; 
+  pfHParTree->Draw("H.fFrac2",fCuts.c_str());
+
   fC3->cd(2);
-  fHParTree->Draw("H.fFrac3",
-		  "H.fGoodImage && H.fTriggerCode==1 && H.fFrac3>0 && "
-		  "H.fPixelsInImage>2");
+  fCuts=fBasicCuts +"&& H.fFrac3>0"; 
+  pfHParTree->Draw("H.fFrac3",fCuts.c_str());
+
   fC3->cd(3);
-  fHParTree->Draw("H.fAsymmetry",
-		  "H.fGoodImage && H.fTriggerCode==1 "
-		  "&& abs(H.fAsymmetry)<1.0 && H.fAsymmetry!=0.0 && "
-		  "H.fPixelsInImage>2");
+  fCuts=fBasicCuts +"&& abs(H.fAsymmetry)<1.0 && H.fAsymmetry!=0.0"; 
+  pfHParTree->Draw("H.fAsymmetry",fCuts.c_str());
+
   fC4->cd(1);
-  fHParTree->Draw("H.fLengthOverSize",
-		  "H.fGoodImage && H.fTriggerCode==1 "
-		  "&& H.fLengthOverSize>0.0 && H.fLengthOverSize<.0015 && "
-		  "H.fPixelsInImage>2");
+  fCuts=fBasicCuts + "&& H.fLengthOverSize>0.0 && H.fLengthOverSize<.0015";
+  pfHParTree->Draw("H.fLengthOverSize",fCuts.c_str());
+
   fC4->cd(2);
-  fHParTree->Draw("H.fLength",
-		  "H.fGoodImage && H.fTriggerCode==1 && H.fLength>0.0 " 
-		  "&& H.fLength<.8 && H.fPixelsInImage>2");
+  fCuts=fBasicCuts + "&& H.fLength>0.0 && H.fLength<.8";
+  pfHParTree->Draw("H.fLength",fCuts.c_str());
+
   fC4->cd(3);
-  fHParTree->Draw("H.fSize",
-		  "H.fGoodImage && H.fTriggerCode==1 && H.fSize>0.0 " 
-                  "&& H.fSize<2000 && H.fPixelsInImage>2");
+  fCuts=fBasicCuts + "&& H.fSize>0.0 && H.fSize<2000"; 
+  pfHParTree->Draw("H.fSize",fCuts.c_str());
 
   fC4->cd(4);
-  fHParTree->Draw("H.fMax3","H.fGoodImage && H.fTriggerCode==1 "
-		  "&& H.fMax3>0.0 && H.fMax3<200 && H.fPixelsInImage>2");
+  fCuts=fBasicCuts + "&& H.fMax3>0.0 && H.fMax3<200";
+  pfHParTree->Draw("H.fMax3",fCuts.c_str());
     
   return;
 }
