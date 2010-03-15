b20fcb5900f444ba3bf0e4f7dc1e88694d91e1c4
sembrosk <sembrosk>
2010-03-15 14:35:50 +0000

    ghs: Updating

--- scripts/PrintEnergySteps.C
+++ scripts/PrintEnergySteps.C
@@ -1,5 +1,5 @@
 //Short root macro to print out energies for equal spacing in log(E)
-
+//Also shows number of shwer we will need at ecah energy
 #include <iomanip>
 
 PrintEnergySteps()
@@ -9,29 +9,33 @@ PrintEnergySteps()
   //double fNumShowersAt100=100;   //gammas
   //double fAlpha=-2.45; //Gamma Ray spectrum index
 
-  double fNumMin=5;   //Cosmic rays
-  double fNumShowersAt100=80;     // protons
-  double fAlpha=-2.77; //Proton spectrum Index
+  //double fNumMin=5;   //Cosmic rays
+  //double fNumShowersAt100=80;     // protons
+  //double fAlpha=-2.77; //Proton spectrum Index
 
   //double fNumMin=5;   //Cosmic rays
   //double fNumShowersAt100=37;     //helium
   //double fAlpha=-2.64;  //Helium spectrum Index
 
-  //int fNumSteps=9;
-  //double fBaseE=.020;    //TeV base energy
-  //int fNumEntries=42;
-  //int fNStart=0;
+  double fNumMin=20;   //Lookup table/effective area: Gammas
+  double fNumShowersAt100=100;   //gammas
+  double fAlpha=-2.0; //Spectrum index
 
-  int fNumSteps=10;
-  double fBaseE=.010;    //TeV base energy
-  int fNumEntries=36;
-  int fNStart=3;
+  int fNumSteps=9;
+  double fBaseE=.020;    //TeV base energy
+  int fNumEntries=42;
+  int fNStart=0;
+
+  //int fNumSteps=10;
+  //double fBaseE=.010;    //TeV base energy
+  //int fNumEntries=36;
+  //int fNStart=3;
 
 
   double fIAlpha=fAlpha+1.0;
   double fStepLogE=(log(.100)-log(fBaseE))/fNumSteps;
   double fLogEBase= log(fBaseE);    
-  int fNumE=85;
+  int fNumE=42;
   double fEOld=.018;
  
   double fE[200];
@@ -43,7 +47,8 @@ PrintEnergySteps()
 
   std::cout<<"Number of steps from "<<fBaseE*1000<<" GeV to 100 GeV: "<<fNumSteps<<std::endl;
   std::cout<<"Spectral Index: "<<fAlpha<<std::endl;
-
+  std::cout<<"---------------------------------------------------"<<std::endl;
+  std::cout<<" Step  E(TeV) LowEdge  High Edge  Number  %BinWidth "<<std::endl;
   for(int i=0;i<fNumE;i++)
     {
       double fLogE=fLogEBase+i*fStepLogE;
