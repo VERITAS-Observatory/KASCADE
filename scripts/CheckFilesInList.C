8b7c8173a86b941b140d32c52fd1f78c17a72c4f
sembrosk <sembrosk>
2012-06-05 13:44:26 +0000

    GHS: Clean up and make safe for humanity.

--- scripts/CheckFilesInList.C
+++ scripts/CheckFilesInList.C
@@ -1,4 +1,4 @@
-void CheckFilesInList(string listOfFiles, string dir)
+void CheckFilesInList(string listOfFiles, string dir,int startLine=1)
 {
   // Check the root files in this list. Just make sure they exist and make sure then 
   // can be loaded.
@@ -8,25 +8,32 @@ void CheckFilesInList(string listOfFiles, string dir)
     cerr<<"Couldn't open "<<listOfFiles<<endl;
     return;
   }
+  int lineNum=0;
 
   while(true){
     char buffer[200];
     infile.getline(buffer, 200);
-    string file =dir + '/' + buffer;
-    cout<<"Testing File: "<<file<<endl;
+    if(infile.eof()){
+      break;
+    }
+    lineNum++;
+    if(lineNum>=startLine){
+      string file =dir + '/' + buffer;
+      cout<<lineNum<<":Testing File: "<<buffer<<endl;
     
-    VARootIO* pfRootIO = new VARootIO(file, true);//open for read only
-    if(!pfRootIO)
-      {
-	cout<<"Couldn't generate a  pointer to VARootIO for "<<file<<endl;
-	continue;
-      }
+      VARootIO* pfRootIO = new VARootIO(file, true);//open for read only
+      if(!pfRootIO)
+	{
+	  cout<<"Couldn't generate a  pointer to VARootIO for "<<file<<endl;
+	  continue;
+	}
 
-    pfRootIO->loadTheRootFile();
-    if(!pfRootIO->IsOpen()){
-      cout<<"Couldn't open "<<file<<endl;
-      cobntinue;
-    } 
+      pfRootIO->loadTheRootFile();
+      if(!pfRootIO->IsOpen()){
+	cout<<"Couldn't open "<<file<<endl;
+	continue;
+      } 
+    }
   }
   return;
 }
