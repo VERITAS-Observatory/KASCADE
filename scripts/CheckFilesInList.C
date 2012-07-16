2b791533ea334dd024ceb8b6cc843e53a2942c9b
sembrosk <sembrosk>
2012-07-16 13:50:43 +0000

    GHS: Updating

--- scripts/CheckFilesInList.C
+++ scripts/CheckFilesInList.C
@@ -1,45 +1,47 @@
-void CheckFilesInList(string listOfFiles, string dir,int startLine=1)
+void CheckFilesInList(string listOfFiles, int start=0)
 {
   // Check the root files in this list. Just make sure they exist and make sure then 
   // can be loaded.
+  int icount=0;
   ifstream infile;
   infile.open(listOfFiles.c_str(), ifstream::in);
   if(!infile.is_open()){
     cerr<<"Couldn't open "<<listOfFiles<<endl;
     return;
   }
-  int lineNum=0;
+  
 
   while(true){
     char buffer[200];
     infile.getline(buffer, 200);
     if(infile.eof()){
+      cout<<"Found EOF of input file "<<listOfFiles<<endl;
       break;
     }
-    lineNum++;
-    if(lineNum>=startLine){
-      string file =dir + '/' + buffer;
-      cout<<lineNum<<":Testing File: "<<buffer<<endl;
-    
-      VARootIO* pfRootIO = new VARootIO(file, true);//open for read only
-      if(!pfRootIO)
-	{
-	  cout<<"Couldn't generate a  pointer to VARootIO for "<<file<<endl;
-	  return 1;
-	}
-
-      pfRootIO->loadTheRootFile();
-      if(!pfRootIO->IsOpen()){
-	cout<<"Couldn't open "<<file<<endl;
-	return 1;
-      } 
-      TTree* pfParameterisedEventTree = pfRootIO->loadTheParameterisedEventTree();
-      if(!pfParameterisedEventTree)
-	{
-	  cout<<"Couldn't find the parameterised event tree for "<<file<<endl;
-	  return 1;
-	}
+    icount++;
+    if(icount<start){
+      continue;
     }
+
+    string file =buffer;
+    cout<<icount<<":Testing File: "<<file<<endl;
+
+    VARootIO* pfRootIO = new VARootIO(file, true);//open for read only
+    if(!pfRootIO)
+      {
+	cout<<"Couldn't generate a  pointer to VARootIO for "<<file<<endl;
+	continue;
+      }
+
+    pfRootIO->loadTheRootFile();
+    if(!pfRootIO->IsOpen()){
+      cout<<"Couldn't open "<<file<<endl;
+      continue;
+    } 
+    pfRootIO->closeTheRootFile();
+    if(pfRootIO!=NULL){
+      delete pfRootIO;
+    }    
   }
   return;
 }
