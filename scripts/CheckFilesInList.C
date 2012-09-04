772bee3c47c62d6fda39614d17ffe6006814204c
sembrosk <sembrosk>
2012-09-04 21:43:38 +0000

    GHS: Update for EAAuto use.

--- scripts/CheckFilesInList.C
+++ scripts/CheckFilesInList.C
@@ -1,41 +1,50 @@
-void CheckFilesInList(string listOfFiles, int start=0)
+int CheckFilesInList(string listOfFiles, int start=0)
 {
-  // Check the root files in this list. Just make sure they exist and make sure then 
+  // Check the root files in this list. Just make sure they exist and make sure they
   // can be loaded.
-  int icount=0;
   ifstream infile;
   infile.open(listOfFiles.c_str(), ifstream::in);
   if(!infile.is_open()){
     cerr<<"Couldn't open "<<listOfFiles<<endl;
-    return;
+    return 1;
   }
   
 
+  int icount=0;
+  int countBad=0;
   while(true){
+    icount++;
     char buffer[200];
     infile.getline(buffer, 200);
     if(infile.eof()){
       cout<<"Found EOF of input file "<<listOfFiles<<endl;
-      break;
+      cout<<"Tested files: "<<start<< "  to "<<icount<<endl;
+      cout<<"Found "<<countBad<<" bad files."<<endl;
+      return countBad;
     }
-    icount++;
     if(icount<start){
-      continue;
+	continue;
     }
 
+    if(icount%500==0){
+      cout<<"At file: "<<icount<<endl;
+    }
     string file =buffer;
-    cout<<icount<<":Testing File: "<<file<<endl;
 
     VARootIO* pfRootIO = new VARootIO(file, true);//open for read only
     if(!pfRootIO)
       {
+	cout<<icount<<":Testing File: "<<file<<endl;
 	cout<<"Couldn't generate a  pointer to VARootIO for "<<file<<endl;
+	countBad++;
 	continue;
       }
 
     pfRootIO->loadTheRootFile();
     if(!pfRootIO->IsOpen()){
+      cout<<icount<<":Testing File: "<<file<<endl;
       cout<<"Couldn't open "<<file<<endl;
+      countBad++;
       continue;
     } 
     pfRootIO->closeTheRootFile();
@@ -43,5 +52,5 @@ void CheckFilesInList(string listOfFiles, int start=0)
       delete pfRootIO;
     }    
   }
-  return;
+  return countBad;
 }
