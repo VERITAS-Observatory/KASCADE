7bdb1743899e1b5e822c52bd59710eaed164bd6e
sembrosk <sembrosk>
2013-03-29 18:41:07 +0000

    GHS: Make new.

---
+++ scripts/CheckStage4FileOK.C
@@ -0,0 +1,56 @@
+void CheckStage4FileOK(string fileName)
+{
+  ofstream os("CheckStage4FileOK.Result");
+  VARootIO* pfRootIO = new VARootIO(fileName.c_str(), true);//open for read only
+  int badCode=0;  
+  os<<badCode<<endl;
+  os.close();
+  if(!pfRootIO) {
+    badCode=1;
+    ofstream os("CheckStage4FileOK.Result");
+    os<<badCode<<endl;
+    os.close();
+    return; 
+  }
+
+  pfRootIO->loadTheRootFile();
+  if(!pfRootIO->IsOpen()){
+    badCode=2;
+    ofstream os("CheckStage4FileOK.Result");
+    os<<badCode<<endl;
+    os.close();
+    return; 
+  } 
+ 
+ //  TTree*  pfParameterisedEventTree = pfRootIO->loadTheParameterisedEventTree();
+  //if(!pfParameterisedEventTree)
+  // {
+  // badCode=3;
+  //ofstream os("CheckStage4FileOK.Result");
+  //os<<badCode<<endl;
+  //os.close();
+  //return; 
+  //}
+  
+  TTree* pfSimulatedEventTree = pfRootIO->loadTheSimulationEventTree();
+  if(!pfSimulatedEventTree)
+    {
+    badCode=4;
+    ofstream os("CheckStage4FileOK.Result");
+    os<<badCode<<endl;
+    os.close();
+    return; 
+    }
+  
+  int numEvents=pfSimulatedEventTree->GetEntries();
+
+  pfRootIO->closeTheRootFile();
+  if(pfRootIO!=NULL){
+    delete pfRootIO;
+  }    
+  badCode=numEvents;
+  ofstream os("CheckStage4FileOK.Result");
+  os<<badCode<<endl;
+  os.close();
+  return; 
+}
