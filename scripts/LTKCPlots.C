e23377c4422ef97bfbbc93eefdd3292ea54b641e
sembrosk <sembrosk>
2015-01-09 16:58:52 +0000

    GHS: Some refinning of lables etc.

--- scripts/LTKCPlots.C
+++ scripts/LTKCPlots.C
@@ -1,4 +1,4 @@
-void LTKCPlots(string pltType, string Zn="20", string Az="180", string kNoise="5.51", string cNoise="5.28",string offSet="0.5")
+void LTKCPlots(string pltType, string Zn="20", string Az="180", string kNoise="5.55", string cNoise="5.21",string offSet="050")
 {
   //string ltkFileName  = "ltk_Jan2013_ua_ATM21_7samples_H_vegas250_alloffsets.root";
   //string ltcFileName  = "$VEGAS/../tables/lt_Oct2012_ua_ATM21_7samples_vegasv250rc5_allOffsets_LZA_noise150fix.root";
@@ -20,8 +20,17 @@ void LTKCPlots(string pltType, string Zn="20", string Az="180", string kNoise="5
   //string ltcFileName  = "$VEGAS/../tables/lt_Oct2012_oa_ATM21_7samples_vegasv250rc5_050wobb_LZA.root";
 
   //SNG All Offset
-  string ltkFileName  = "lt_MDL15NA_V5_T1Move_ATM22_KASCADE_vegasv250rc5_7sam_Alloff_std_d1.43_LZA.root";
-  string ltcFileName  = "$VEGAS/../tables/lt_Oct2012_na_ATM22_7samples_vegasv250rc5_allOffsets_LZA.root";
+  //string ltkFileName  = "lt_MDL15NA_V5_T1Move_ATM22_KASCADE_vegasv250rc5_7sam_Alloff_std_d1.43_LZA.root";
+  //string ltcFileName  = "$VEGAS/../tables/lt_Oct2012_na_ATM22_7samples_vegasv250rc5_allOffsets_LZA.root";
+
+  //SUG All Offset
+  //string ltkFileName  = "lt_MDL10UA_V6_PMTUpgrade_ATM22_KASCADE_vegasv250rc5_7sam_Alloff_std_d1.43_LZA.root";
+  //string ltcFileName  = "$VEGAS/../tables/lt_Oct2012_ua_ATM22_7samples_vegasv250rc5_allOffsets_LZA_noise150fix.root";
+
+ //SUG 050 Offset
+  string ltkFileName  = "lt_MDL10UA_V6_PMTUpgrade_ATM22_KASCADE_vegasv250rc5_7sam_050off_std_d1.43_LZA.root";
+  string ltcFileName  = "$VEGAS/../tables/lt_Oct2012_ua_ATM22_7samples_vegasv250rc5_050wobb_LZA_noise150fix.root";
+
 
 
   if (offSet != "050" ) {
@@ -46,23 +55,28 @@ void LTKCPlots(string pltType, string Zn="20", string Az="180", string kNoise="5
   c1->Clear();
   c1->Divide(2,2);
   c1->cd(1);
-  widthTest(ltkFileName.c_str(),LTK_LT.c_str(),50," ",1, pltType.c_str());
-  widthTest(ltcFileName.c_str(),LTC_LT.c_str(),50,"same", 2, pltType.c_str());
+  string pltTitleBase =pltType+" " + Zn + "_" + Az+ " " + kNoise + " " + offSet;
+  string pltTitle = pltTitleBase + " 50m";
+  widthTest(ltkFileName.c_str(),LTK_LT.c_str(),50," ",1, pltTitle.c_str());
+  widthTest(ltcFileName.c_str(),LTC_LT.c_str(),50,"same", 2, pltTitle.c_str());
 
   c1->cd(2);
-  widthTest(ltkFileName.c_str(),LTK_LT.c_str(),75," ",1, pltType.c_str());
-  widthTest(ltcFileName.c_str(),LTC_LT.c_str(),75,"same", 2, pltType.c_str());
+  pltTitle = pltTitleBase + " 75m";
+  widthTest(ltkFileName.c_str(),LTK_LT.c_str(),75," ",1, pltTitle.c_str());
+  widthTest(ltcFileName.c_str(),LTC_LT.c_str(),75,"same", 2, pltTitle.c_str());
 
   c1->cd(3);
-  widthTest(ltkFileName.c_str(),LTK_LT.c_str(),100," ",1, pltType.c_str());
-  widthTest(ltcFileName.c_str(),LTC_LT.c_str(),100,"same", 2, pltType.c_str());
+  pltTitle = pltTitleBase + " 100m";
+  widthTest(ltkFileName.c_str(),LTK_LT.c_str(),100," ",1, pltTitle.c_str());
+  widthTest(ltcFileName.c_str(),LTC_LT.c_str(),100,"same", 2, pltTitle.c_str());
 
   c1->cd(4);
-  widthTest(ltkFileName.c_str(),LTK_LT.c_str(),150," ", 1, pltType.c_str());
-  widthTest(ltcFileName.c_str(),LTC_LT.c_str(),150,"same", 2, pltType.c_str());
+  pltTitle = pltTitleBase + " 150m";
+  widthTest(ltkFileName.c_str(),LTK_LT.c_str(),150," ", 1, pltTitle.c_str());
+  widthTest(ltcFileName.c_str(),LTC_LT.c_str(),150,"same", 2, pltTitle.c_str());
  
-  cout<<"Black: "<<ltkFileName<<endl;
-  cout<<"Red: "<<ltcFileName<<endl;
+  cout<<"Black: "<<ltkFileName<<"/"<< LTK_LT<<endl;
+  cout<<"Red: "<<ltcFileName<<"/"<< LTC_LT<<endl;
 
  return;
 }
